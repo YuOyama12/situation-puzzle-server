bf12e23a999c2ddb0a9587a6a9d7df0af168c9de
YuOyama12 <yuoyama.5@gmail.com>
2025-05-27 20:26:31 +0900

    404エラーをrepositoryの中で投げるように変更

--- api/routers/quiz.py
+++ api/routers/quiz.py
@@ -1,7 +1,7 @@
 from typing import List
 from data.database.db import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from api.schemas.quiz import *
 from api.schemas.result_response import *
 from usecase.quiz_use_case import QuizUseCase 
@@ -45,10 +45,6 @@ async def get_quiz(
     db: AsyncSession = Depends(get_db)
 ):
     result = await quiz_use_case.fetch_quiz_by_id(id = quiz_id, db = db)
-
-    if result is None:
-        raise HTTPException(status_code=404, detail="quiz_not_found")
-    
     return result
 
 
