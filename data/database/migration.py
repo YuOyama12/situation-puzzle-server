88d98a09908f8faa137938dba1e10feaadde6b5b
YuOyama12 <yuoyama.5@gmail.com>
2025-05-31 08:57:40 +0900

    現状のmigrationファイルの書き方ではquizzesテーブルしか生成されないので修正

--- data/database/migration.py
+++ data/database/migration.py
@@ -1,12 +1,14 @@
+from api.models.quiz import Quiz
+from api.models.user import User
 from api.settings import DB_URL
 from sqlalchemy import create_engine
-from api.models.quiz import Base as Quiz
+from data.database.db import Base
 
 engine = create_engine(DB_URL, echo=True)
 
 def reset_database():
-    Quiz.metadata.drop_all(bind=engine)
-    Quiz.metadata.create_all(bind=engine)
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
 
 if __name__ == "__main__":
     reset_database()
