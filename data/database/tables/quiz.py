9ac400a925843dc26a979bde36f78e743341fdb7
YuOyama12 <yuoyama.5@gmail.com>
2025-06-11 21:44:13 +0900

    quizzesテーブルに作問者情報保存のためにユーザーIDカラムを追加

--- data/database/tables/quiz.py
+++ data/database/tables/quiz.py
@@ -1,5 +1,5 @@
 from .core import TimestampMixin
-from sqlalchemy import Column, String, Text
+from sqlalchemy import BIGINT, Column, String, Text
 from data.database.db import Base
 
 import uuid
@@ -8,6 +8,7 @@ class Quiz(Base, TimestampMixin):
     __tablename__ = "quizzes"
 
     id = Column(String(36), primary_key=True, default=uuid.uuid4)
+    user_id = Column(BIGINT, default=0)
     title = Column(String(24))
     question = Column(Text)
     answer = Column(Text)
