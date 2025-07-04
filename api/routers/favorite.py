f2fd08fc4f52f75e9f4a179cab97d7d0dc0885a3
YuOyama12 <yuoyama.5@gmail.com>
2025-07-05 08:01:40 +0900

    お気に入り登録削除APIを追加

---
+++ api/routers/favorite.py
@@ -0,0 +1,59 @@
+from data.database.db import get_db
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import APIRouter, Depends, HTTPException, Header
+from api.schemas.quiz import *
+from api.schemas.result_response import *
+from domain.constants import ErrorMessages
+from usecase.favorite_use_case import FavoriteUseCase
+
+
+router = APIRouter()
+
+favorite_use_case = FavoriteUseCase()
+
+API_FAVORITE_TAG = "favorite" 
+
+@router.post(
+        "/favorites/{quiz_id}",
+        response_model = ResultResponse,
+        tags = [API_FAVORITE_TAG],
+        description = "問題お気に入り追加API"
+    )
+async def add_to_favorites(
+    quiz_id: str,
+    user_id: int = Header(None),
+    db: AsyncSession = Depends(get_db)
+):
+    if (user_id is None):
+        raise HTTPException(status_code=401, detail=ErrorMessages.AUTH_FAILED)
+    
+    await favorite_use_case.add_to_favorites(
+        db=db,
+        user_id=user_id,
+        quiz_id=quiz_id,
+    )
+
+    return ResultResponse(code=200, message="OK")
+
+
+@router.delete(
+        "/favorites/{quiz_id}",
+        response_model = ResultResponse,
+        tags = [API_FAVORITE_TAG],
+        description = "問題お気に入り削除API"
+    )
+async def remove_from_favorites(
+    quiz_id: str,
+    user_id: int = Header(None),
+    db: AsyncSession = Depends(get_db)
+):
+    if (user_id is None):
+        raise HTTPException(status_code=401, detail=ErrorMessages.AUTH_FAILED)
+    
+    await favorite_use_case.remove_from_favorites(
+        db=db,
+        user_id=user_id,
+        quiz_id=quiz_id,
+    )
+
+    return ResultResponse(code=200, message="OK")
--- domain/constants.py
+++ domain/constants.py
@@ -2,6 +2,7 @@ MAX_QUIZ_COUNT_AS_NEW_ARRIVAL = 3
 
 class ErrorMessages:
     QUIZ_NOT_FOUND = "お探しの問題が見つかりませんでした。"
+    FAVORITE_ALREADY_REMOVED = "対象はお気に入りからすでに外れています。"
     LOGIN_FAILED = "ログインに失敗しました。ユーザーID、またはパスワードが間違っている可能性があります。"
     AUTH_FAILED = "ログイン状態が確認できませんでした、セッションが切断された可能性があります。再度ログインしなおしてください。"
     USER_NAME_DUPLICATION = "このユーザー名は既に使用されています。"
